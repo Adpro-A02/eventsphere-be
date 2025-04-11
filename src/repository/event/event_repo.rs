b2161f505676d398dc22fc5a05374863a243db5f
Priyapta <priaptanaufal@gmail.com>
2025-04-11 19:58:25 +0700

    [GREEN] fix implement event repository

--- src/repository/event/event_repo.rs
+++ src/repository/event/event_repo.rs
@@ -32,20 +32,42 @@ impl InMemoryEventRepo {
 }
 
 impl EventRepository for InMemoryEventRepo {
-    fn add(&self, event: Event) -> Result<Event, String>{
-        todo!();
+    fn add(&self, event: Event) -> Result<Event, String> {
+        let mut events = self.events.lock().map_err(|_| "Lock poisoned".to_string())?;
+        let id_str = event.id.to_string();
+        if events.contains_key(&id_str) {
+            return Err("Event already exists".to_string());
+        }
+        events.insert(id_str, event.clone());
+        Ok(event)
     }
-    fn delete(&self, event_id: Uuid) -> Result<(), String>{
-        todo!();
+    fn delete(&self, event_id: Uuid) -> Result<(), String> {
+        let mut events = self.events.lock().map_err(|_| "Lock poisoned".to_string())?;
+        let id_str = event_id.to_string();
+        if events.remove(&id_str).is_some() {
+            Ok(())
+        } else {
+            Err("Event not found".to_string())
+        }
     }
-    fn update_event(&self, event_id: Uuid, updated_event: Event) -> Result<Event, String>{
-        todo!();
+    fn update_event(&self, event_id: Uuid, updated_event: Event) -> Result<Event, String> {
+        let mut events = self.events.lock().map_err(|_| "Lock poisoned".to_string())?;
+        let id_str = event_id.to_string();
+        if events.contains_key(&id_str) {
+            events.insert(id_str, updated_event.clone());
+            Ok(updated_event)
+        } else {
+            Err("Event not found".to_string())
+        }
     }
-    fn list_events(&self) -> Result<Vec<Event>, String>{
-        todo!();
+    fn list_events(&self) -> Result<Vec<Event>, String> {
+        let events = self.events.lock().map_err(|_| "Lock poisoned".to_string())?;
+        Ok(events.values().cloned().collect())
     }
-    fn get_by_id(&self, event_id: Uuid) -> Result<Option<Event>, String>{
-        todo!();
+
+    fn get_by_id(&self, event_id: Uuid) -> Result<Option<Event>, String> {
+        let events = self.events.lock().map_err(|_| "Lock poisoned".to_string())?;
+        Ok(events.get(&event_id.to_string()).cloned())
     }
    
 }
