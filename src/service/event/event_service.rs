b7e7f8d76a4c0253aa2d7b2245c34f8390e2d5b2
Priyapta <priaptanaufal@gmail.com>
2025-04-11 16:11:21 +0700

    [REFACTOR] fix skeleton event service

--- src/service/event/event_service.rs
+++ src/service/event/event_service.rs
@@ -4,6 +4,16 @@ use tokio::runtime::Runtime;
 use crate::model::event::Event;
 use crate::repository::event::EventRepository;
 
+
+pub trait EventService {
+    fn create_event(&self, event: Event) -> Result<Event, ServiceError>;
+    fn list_events(&self) -> Result<Vec<Event>, ServiceError>;
+    fn get_event(&self, event_id: &str) -> Result<Event, ServiceError>;
+    fn update_event(&self, event_id: &str, event: Event) -> Result<Event, ServiceError>;
+    fn delete_event(&self, event_id: &str) -> Result<(), ServiceError>;
+}
+
+
 #[derive(Debug)]
 pub enum ServiceError {
     RepositoryError(String),
@@ -17,15 +27,14 @@ impl From<String> for ServiceError {
     }
 }
 
-pub struct EventService<R: EventRepository> {
+pub struct DefaultEventService<R: EventRepository> {
     repository: Arc<R>,
     runtime: Runtime,
 }
 
-impl<R: EventRepository> EventService<R> {
+impl<R: EventRepository> DefaultEventService<R>{
     pub fn new(repository: Arc<R>) -> Self {
-        let runtime = Runtime::new().expect("Failed to create Tokio runtime");
-        Self { repository, runtime }
+        todo!()
     }
 
     pub fn create_event(&self, event: Event) -> Result<Event, ServiceError> {
